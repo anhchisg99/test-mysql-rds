e5650e54087dd16705872ee0a913f1e0cef1ad2b
chi_duong <anhchictw311@gmail.com>
2022-11-06 00:25:38 +0700

    fix rds

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ import mysql from 'mysql'
 
 const port = 3001
 let connection = mysql.createConnection({
-    host: '172.16.4.154',
-    user: 'root',
-    password: '123456',
+    host: 'company.cw7hhrixt5qj.ap-southeast-1.rds.amazonaws.com',
+    user: 'admin',
+    password: '12345678',
     database: 'company'
 })
 
@@ -16,7 +16,51 @@ let connection = mysql.createConnection({
 app.get('/',(req,res)=>{
     res.send('success')
 })
-app.get('/api',(req,res)=>{
+app.get('/create-table',(req,res)=>{
+    connection.connect(function(err){
+        if(err){
+            return console.error('error: ' + err.message);
+        }
+        // let createTable  = `create table admin (name varchar(20),age int)`;
+        let showUserTable = `create table users (name varchar(20),age int)`;
+        connection.query(showUserTable,function(err,results){
+            if(err){
+                console.log({err})
+            }else{
+                res.send({results,'state':'success'})
+            }
+        })
+        connection.end(function(err){
+            if(err){
+                return console.log(err)
+            }
+        })
+    })
+    // res.send()
+})
+app.get('/insert-table',(req,res)=>{
+    connection.connect(function(err){
+        if(err){
+            return console.error('error: ' + err.message);
+        }
+        // let createTable  = `create table admin (name varchar(20),age int)`;
+        let showUserTable = `insert into users values('cuong',12))`;
+        connection.query(showUserTable,function(err,results){
+            if(err){
+                console.log({err})
+            }else{
+                res.send({results,'state':'success'})
+            }
+        })
+        connection.end(function(err){
+            if(err){
+                return console.log(err)
+            }
+        })
+    })
+    // res.send()
+})
+app.get('/get-table',(req,res)=>{
     connection.connect(function(err){
         if(err){
             return console.error('error: ' + err.message);
