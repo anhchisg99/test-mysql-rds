5d71ef189f106033cd3ed08333aa0cfced5e4129
chi_duong <anhchictw311@gmail.com>
2022-11-05 21:08:42 +0700

    initiate

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from 'express'
+const app = express()
+import mysql from 'mysql'
+
+const port = 3001
+let connection = mysql.createConnection({
+    host: '172.16.4.154',
+    user: 'root',
+    password: '123456',
+    database: 'company'
+})
+
+
+
+
+app.use('/',(req,res)=>{
+    res.send('success')
+})
+app.use('/api',(req,res)=>{
+    connection.connect(function(err){
+        if(err){
+            return console.error('error: ' + err.message);
+        }
+        // let createTable  = `create table admin (name varchar(20),age int)`;
+        let showUserTable = `select * from users`;
+        connection.query(showUserTable,function(err,results){
+            if(err){
+                console.log({err})
+            }else{
+                res.send(results)
+            }
+        })
+        connection.end(function(err){
+            if(err){
+                return console.log(err)
+            }
+        })
+    })
+    // res.send()
+})
+
+app.listen(port, () => console.log(`listen on ${port} !!!`))
